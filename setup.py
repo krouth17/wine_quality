cb122cec84cc898955269077a9d3c80f8e9524f7
Kamalesh <kamalesh.routh@tigeranalytics.com>
2021-04-13 18:31:41 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prediction of wine quality',
+    author='krouth17',
+    license='MIT',
+)
